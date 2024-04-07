834d62eeb489e867613072e5c5921e0b7f957848
Nitish-B2M <nitishssharma20@gnu.ac.in>
2024-04-07 12:11:55 +0530

    Refactor DashboardCard component and Dashboard.scss for improved styling and readability

--- src/components/Dashboard/DashBoardCard.jsx
+++ src/components/Dashboard/DashBoardCard.jsx
@@ -1,12 +1,8 @@
 const DashboardCard = ({ title, value, name }) => {
     return (
-        <div className="cards">
-        <div className="carditem">
-            <div className="cardtitle">{title}</div>
-            <div className="cardname">{name}</div>
-            <div className="cardcount">{value}</div>
-        </div>
-        </div>
+        <>
+            {title} {value}<br />
+        </>
     );
 };
 
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,73 +1,158 @@
-// import React from 'react';
+import React, { useEffect, useState } from "react";
 import DashboardCard from "./DashBoardCard";
 import "./Dashboard.scss";
 import 'datatables.net-dt/css/jquery.dataTables.min.css';
+import { useAuthContext } from "../../hooks/useAuthContext";
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-dt/css/jquery.dataTables.min.css';
+import { formatDistanceToNow } from 'date-fns';
 
 
-const Dashboard = () => {
-  
-  
+const Dashboard = ({ setDashboardData, activeDashboardItemLink }) => {
+  // context
+  const { user } = useAuthContext();
+  const [data, setData] = useState([]);
+  const [getUsers, setGetUsers] = useState([]);
+  const [getProjects, setGetProjects] = useState([]);
+  const [getIssues, setGetIssues] = useState([]);
+  const [getPublicIssues, setGetPublicIssues] = useState([]);
+  useEffect(() => {
+    if ( !user || !user.isLoggedIn ) return;
+    if ( user && user.role !== "admin" ) return;
+    fetch("http://localhost:3300/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        data.data.forEach((user) => {
+          user.updatedAt = formatDistanceToNow(new Date(user.updatedAt), { addSuffix: true });
+        });
+        setGetUsers(data.data)
+      });
 
+    fetch("http://localhost:3300/api/projects")
+      .then((res) => res.json())
+      .then((data) => {
+        data.data.forEach((project) => {
+          project.updatedAt = formatDistanceToNow(new Date(project.updatedAt), { addSuffix: true });
+        });
+        setGetProjects(data.data)});
 
-  // only one time fetch data
-  // useEffect(() => {
-  //   fetch("api/users")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setuserData(data);
-  //     })
-  // }, [random]);
+    fetch("http://localhost:3300/api/issues")
+      .then((res) => res.json())
+      .then((data) => {
+        data.data.forEach((issue) => {
+          issue.updatedAt = formatDistanceToNow(new Date(issue.updatedAt), { addSuffix: true });
+        });
+        setGetIssues(data.data)});
 
-//     "username": "username",
-//     "password": "password",
-//     "email": "email",
-//     "role": "role",
-//     "status": "status",
+    fetch("http://localhost:3300/api/public-issues")
+      .then((res) => res.json())
+      .then((data) => {
+        data.data.forEach((publicissue) => {
+          publicissue.updatedAt = formatDistanceToNow(new Date(publicissue.updatedAt), { addSuffix: true });
+        });
+        setGetPublicIssues(data.data)});
 
-  // const issueData = [
-  //   issuedata.issues
-  // ]
+    if(setDashboardData){
+    setDashboardData({
+      users: getUsers.length,
+      projects: getProjects.length,
+      issues: getIssues.length,
+      publicIssues: getPublicIssues.length,
+    })}
+  }, []);
+
+  useEffect(() => {
+    if ( activeDashboardItemLink === "users" ) {
+      setDataToTable("users", getUsers);
+    } else if ( activeDashboardItemLink === "projects" ) {
+      setDataToTable("projects", getProjects);
+    } else if ( activeDashboardItemLink === "issues" ) {
+      setDataToTable("issues", getIssues);
+    } else if ( activeDashboardItemLink === "public-issues" ) {
+      setDataToTable("public-issues", getPublicIssues);
+    }
+  }, [activeDashboardItemLink]);
+
+  const setDataToTable = (title, data) => {
+    if(title === "users"){
+      $("#showContent").DataTable({
+        data: data,
+        columns: [
+          { title: "Name", data: "username" },
+          { title: "Email", data: "email" },
+          { title: "Role", data: "role" },
+          { title: "Last Seen", data: "updatedAt" },
+        ],
+        "bDestroy": true,
+      });
+
+    }else if(title === "projects"){
+      $("#showContent").DataTable({
+        data: data,
+        columns: [
+          { data: "title", title: "Title" },
+          { data: "description", title: "Description" },
+          { data: "department", title: "Department" },
+          { data: "updatedAt", title: "Updated At" },
+        ],
+        "bDestroy": true,
+      });
+    }else if(title === "issues"){
+      $("#showContent").DataTable({
+        data: data,
+        columns: [
+          { title: "Title", data: "title" },
+          { title: "Description", data: "description" },
+          { title: "Project", data: "project_id.title" },
+          { title: "Updated At", data: "updatedAt" },
+        ],
+        "bDestroy": true,
+      });
+    
+    
+
+    }
+    
+  }
+
   
   
   return (
     <div className="dashboard container">
       <div className="dashheader d-a-flex">
         <div className="dashname">Dashboard</div>
-        <div className="dashsearch d-a-flex">
-          <input type="text" className="c-dash-comp1" placeholder="Search" />
-          <button className="c-dash-comp1">Search</button>
-        </div>
       </div>
 
       <div className="dashcontent">
-        <div className="countcards">
+        {/* <div className="countcards">
           <div className="dashboard-title">
             <div className="cardtitle">Bugs/Issues</div>
           </div>
+          
 
-          <DashboardCard title="Bugs" value="1K" name={"Total"} />
-          <DashboardCard title="Issues" value="98" name={"Open"} />
-          <DashboardCard title="Issues" value="908" name={"Closed"} />
-        </div>
-
+        </div> */}
+      { user && user.role === "admin" ?
         <div className="dashreport">
           <div className="reportcard">
             <div className="report-title dashboard-title">Reports</div>
             <div className="reportcontent d-a-flex">
-              <div className="reportGraphButton d-a-flex">
-                <span>Project</span>
-                <span>Issues</span>
-                <span>Resolved</span>
-              </div>
+              {/* <div className="reportGraphButton d-a-flex">
+                <span id="users" className="btn btn-button active" onClick={() => {setDataToTable(activeDashboardItemLink, getUsers)}}>Users</span>
+                <span id="projects" className="btn btn-button" onClick={() => {setDataToTable(activeDashboardItemLink, getProjects)}}>Projects</span>
+                <span id="issues" className="btn btn-button" onClick={() => {setDataToTable(activeDashboardItemLink, getIssues)}}>Issues</span>
+                <span id="pissues" className="btn btn-button" onClick={() => {setDataToTable(activeDashboardItemLink, getPublicIssues)}}>Public Bugs</span>
+              </div> */}
               <div className="reportGraph">
                 <div className="reportGraphItem">
-                  <img src="https://via.placeholder.com/150" alt="graph" />
+                  <table id="showContent" className="table table-striped table-bordered"></table>
                 </div>
               </div>
             </div>
           </div>
         
         </div>
+        : null }
           
       </div>
     </div>
